4fd41aca88fc18c6e09e460ae13c1d984a907d0c
saniyar <saniyar@hamravesh.com>
2023-05-07 16:10:21 +0330

    [ TEST ] github shared runners

--- script.py
+++ script.py
@@ -16,6 +16,15 @@ github_data = {
         }
     },
     "jobs": {
+        "build": {
+            "runs-on": "ubuntu-latest",
+            "steps": [
+                {
+                    "name": "Checkout code",
+                    "uses": "actions/checkout@v2"
+                }
+            ]
+        }
     }
 }
 
